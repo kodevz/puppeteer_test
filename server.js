99fb93143570d18505167f52a92826053d918279
kodevz <karthi.php.developer@gmail.com>
2021-03-03 20:24:51 +0530

    Changes

--- server.js
+++ server.js
@@ -20,8 +20,10 @@ app.get('/preview', async (req, res) => {
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://www.currys.co.uk/gbuk/tv-and-home-entertainment/televisions/televisions/hisense-50a7300ftuk-50-smart-4k-ultra-hd-hdr-led-tv-10207138-pdt.html');
-    await page.screenshot({path: 'public/screenshot.png'});
+    await page.goto('https://news.ycombinator.com', {
+        waitUntil: 'networkidle2',
+    });
+    await page.screenshot({ path: 'public/screenshot.png' });
     browser.close();
 
     res.render('content.html', { screenShot: 'screenshot.png' });
@@ -36,5 +38,5 @@ app.get('/', async (req, res) => {
 
 // Making Express listen on port 7000
 app.listen(port, function () {
-    console.log('Running on port 3000.',port);
+    console.log('Running on port 3000.', port);
 });
