6bc72ef0cadd41e62b429e35d75f639dff3c3e7b
kodevz <karthi.php.developer@gmail.com>
2021-03-03 20:10:56 +0530

    Changes

--- server.js
+++ server.js
@@ -17,15 +17,15 @@ var port = process.env.PORT || 8080;
 
 app.get('/', async (req, res) => {
 
-    // const browser = await puppeteer.launch();
-    // const page = await browser.newPage();
-    // await page.goto('https://www.currys.co.uk/gbuk/tv-and-home-entertainment/televisions/televisions/hisense-50a7300ftuk-50-smart-4k-ultra-hd-hdr-led-tv-10207138-pdt.html');
-    // await page.screenshot({path: 'public/screenshot.png'});
-    // browser.close();
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.currys.co.uk/gbuk/tv-and-home-entertainment/televisions/televisions/hisense-50a7300ftuk-50-smart-4k-ultra-hd-hdr-led-tv-10207138-pdt.html');
+    await page.screenshot({path: 'public/screenshot.png'});
+    browser.close();
 
-    // res.render('content.html', { screenShot: 'screenshot.png' });
+    res.render('content.html', { screenShot: 'screenshot.png' });
 
-    res.sendFile(path.join(__dirname + '/index.html'));
+    // res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 
